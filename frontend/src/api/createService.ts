bbdaa65be129da233b6f484f4e35e4a8d7df4117
Freek Gruntjes <freek@hipex.io>
2023-09-03 22:31:27 +0200

    Removed old api integration

--- frontend/src/api/createService.ts
+++
@@ -1,7 +0,0 @@
-import { createServiceConfiguration } from './createServiceConfiguration';
-import type { Configuration } from './generated';
-
-export function createService<T>(type: new (configuration: Configuration) => T): T {
-	const configuration = createServiceConfiguration();
-	return new type(configuration);
-}
