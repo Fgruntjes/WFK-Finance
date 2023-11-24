82698184bc6f5cfe55f0d6650197bec8c2112fe4
Freek Gruntjes <freek@hipex.io>
2023-11-24 17:42:23 +0100

    Fix database admin login_username

--- terraform/backend_database.tf
+++ terraform/backend_database.tf
@@ -8,10 +8,10 @@ resource "azurerm_mssql_server" "backend_database" {
   public_network_access_enabled        = true
 
   azuread_administrator {
-    azuread_authentication_only = var.app_environment == "main"
+    azuread_authentication_only = true
     tenant_id                   = var.arm_tenant_id
     object_id                   = var.arm_client_id
-    login_username              = "appadmin"
+    login_username              = "github-actions-${var.app_project_slug}"
   }
 
   identity {
