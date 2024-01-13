067bc8e00d3df12b0afc2a33906d8ef2cdedc9e2
Freek Gruntjes <freek@hipex.io>
2024-01-13 13:47:29 +0100

    frontend build fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -14,9 +14,7 @@ export default defineConfig(({ mode }) => {
 
   function envVal(name: string) {
     if (!process.env[name]) {
-      throw new Error(
-        `Env var missing '${name}', did you run 'deploy.sh' to create dev cloud resources?`,
-      );
+      return `__MISSING_ENV_VAR__${name}__`;
     }
 
     return JSON.stringify(process.env[name]);
